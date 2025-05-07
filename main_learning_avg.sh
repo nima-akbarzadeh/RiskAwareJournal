feff2ac12f235e57560decbb0db9e8da9e5b4191
nima <nima.akbarzadeh@mail.mcgill.ca>
2025-05-07 09:31:30 -0400

    .sh file is added.

---
+++ main_learning_avg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --mail-user=nima.akbarzadeh@mail.mcgill.ca
+#SBATCH --account=def-adulyasa
+#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=21
+#SBATCH --output=~/projects/def-adulyasa/mcnima/RiskAwareJournal/output.txt
+#SBATCH --time=01:00:00
+
+module load python/3.10
+
+source ~/envs/restless_bandits/bin/activate
+
+python main_learning_avg.py
